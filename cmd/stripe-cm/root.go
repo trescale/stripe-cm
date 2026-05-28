c4cd34094d25c6876e78e238822b60ac0697aedf
Rohil Surana <rohilsurana96@gmail.com>
2026-05-28 16:28:01 +0530

    fix: resolve version from Go build info for go install

--- cmd/stripe-cm/root.go
+++ cmd/stripe-cm/root.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -11,6 +12,24 @@ var (
 	commit         = "none"
 	stripeYAMLFile string
 )
+
+func resolveVersion() {
+	if version != "dev" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		version = info.Main.Version
+	}
+	for _, s := range info.Settings {
+		if s.Key == "vcs.revision" && len(s.Value) >= 7 {
+			commit = s.Value[:7]
+		}
+	}
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "stripe-cm",
@@ -26,6 +45,7 @@ var versionCmd = &cobra.Command{
 }
 
 func init() {
+	resolveVersion()
 	rootCmd.PersistentFlags().StringVar(&stripeYAMLFile, "config", "stripe.yaml", "Path to Stripe configuration YAML file")
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.Version = version
